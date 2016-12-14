72aa24ac391b289afb0a75c84da618cd45758156
Slava <rtootik@gmail.com>
2016-12-14 13:39:45 +0300

    Initial Commit

---
+++ ArrayHomeWork01/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ArrayHomeWork01
+//
+//  Created by Slava on 14.12.16.
+//  Copyright © 2016 Slava. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
